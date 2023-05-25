c71b44195b00174eb88a043e9e585e2970b572ef
ronak <ronak@hoicko.net>
2023-05-25 21:47:38 +0530

    change

---
+++ src/pages/UserMangement/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+
+const UserManagement = () => {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <div>UserManageMent</div>
+    </DashboardLayout>
+  );
+};
+
+export default UserManagement;
